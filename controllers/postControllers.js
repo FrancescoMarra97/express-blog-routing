547534e1a3d8dcd4598a33b3e1039a6170842ba1
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-04 17:30:22 +0100

    slug

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,9 +1,23 @@
 const posts = require('../db/db.js')
 
+const show = (req, res) => {
+    const slug = req.params.slug
+
+    const post = posts.find((post) => post.slug === slug)
+
+    if (!post) {
+        return res.status(404).json({
+            message: '404! not found'
+        })
+    }
+
+    res.status(200).json(post)
+}
+
 const index = (req, res) => {
-	let html = '<ul>'
-	posts.forEach((post) => {
-		html += `
+    let html = '<ul>'
+    posts.forEach((post) => {
+        html += `
 		<li>
 			<h2>${post.title}</h2>
             <img src="/imgs/posts/${post.image}" alt="${post.title}">
@@ -11,7 +25,9 @@ const index = (req, res) => {
             ${post.tags.join(', ')}
 		</li>
 		`
-	})
-	html += '</ul>'
-	res.send(html)
+    })
+    html += '</ul>'
+    res.send(html)
 }
+
+module.exports = { show, index }
